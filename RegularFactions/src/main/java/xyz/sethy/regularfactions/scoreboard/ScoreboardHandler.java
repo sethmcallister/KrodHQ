96b7233778dc5260621756e328193a02c976df22
GenuineAlex <alexgalaxydev@gmail.com>
2017-03-26 15:29:24 +0100

    Added Scoreboard

---
+++ RegularFactions/src/main/java/xyz/sethy/regularfactions/scoreboard/ScoreboardHandler.java
@@ -0,0 +1,53 @@
+package xyz.sethy.regularfactions.scoreboard;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import xyz.sethy.api.API;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by sethm on 21/12/2016.
+ */
+public class ScoreboardHandler implements Listener
+{
+    private ConcurrentHashMap<Player, Scoreboard> scoreboards;
+
+    public ScoreboardHandler()
+    {
+        this.scoreboards = new ConcurrentHashMap<>();
+        Bukkit.getServer().getPluginManager().registerEvents(this, API.getPlugin());
+    }
+
+    public Scoreboard getScoreboard(Player player)
+    {
+        return scoreboards.get(player);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event)
+    {
+        org.bukkit.scoreboard.Scoreboard scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+        event.getPlayer().setScoreboard(scoreboard);
+
+        Scoreboard scoreboard1 = new Scoreboard(scoreboard, "&cKrodHQ Factions");
+        scoreboards.put(event.getPlayer(), scoreboard1);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event)
+    {
+        scoreboards.remove(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onPlayerKick(PlayerKickEvent event)
+    {
+        scoreboards.remove(event.getPlayer());
+    }
+}
---
+++ RegularFactions/src/main/java/xyz/sethy/regularfactions/scoreboard/ScoreboardThread.java
@@ -0,0 +1,52 @@
+package xyz.sethy.regularfactions.scoreboard;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import xyz.sethy.api.API;
+import xyz.sethy.api.framework.user.User;
+import xyz.sethy.hub.Hub;
+import xyz.sethy.hub.queue.PlayerQueue;
+import xyz.sethy.hub.server.Server;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by sethm on 21/12/2016.
+ */
+public class ScoreboardThread extends BukkitRunnable
+{
+    private final DecimalFormat FORMAT = new DecimalFormat("0.0");
+    private final PlayerQueue playerQueue = Hub.getInstance().getPlayerQueue();
+
+    @Override
+    public void run()
+    {
+        for (Player player : Hub.getInstance().getPlayers())
+        {
+            Scoreboard scoreboard = Hub.getInstance().getScoreboardHandler().getScoreboard(player);
+            if (scoreboard == null)
+                continue;
+
+            scoreboard.clear();
+            User user = API.getUserManager().findByUniqueId(player.getUniqueId());
+
+            scoreboard.add(translateString("&7&m-----------"), translateString("&7&m-----------"));
+
+            
+
+            scoreboard.add(translateString("&7&m-----------"), translateString("&7&m-----------"));
+            scoreboard.update();
+        }
+    }
+
+    private String format(long millisecond)
+    {
+        return FORMAT.format(millisecond / 1000.0D);
+    }
+
+    private String translateString(String string)
+    {
+        return ChatColor.translateAlternateColorCodes('&', string);
+    }
+}
